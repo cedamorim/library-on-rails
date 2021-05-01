1359f93178198a1a77be87ad5dd8d4f3ed10dd6e
Carlos Eduardo <cedamorim@gmail.com>
2021-04-30 23:40:26 -0300

    Adicionado o Faker e criado configurado seed para usuário e livros

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!({
+               name: 'Administrador',
+               email: 'admin@admin.com',
+               password: '123456'
+             })
+
+200.times do
+  Book.create!({
+                 title: Faker::Book.title,
+                 author: Faker::Book.author,
+                 image_url: Faker::Placeholdit.image(size: '100x100'),
+                 description: Faker::Movies::BackToTheFuture.quote
+               })
+end
