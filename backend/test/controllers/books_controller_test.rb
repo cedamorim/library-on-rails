3823d2ab11c3562430e1ba560fb4bd030576f079
Carlos Eduardo <cedamorim@gmail.com>
2021-05-01 14:50:39 -0300

    Adicionado cors, corrigido a rota de book para books, corrigido os testes também

--- backend/test/controllers/books_controller_test.rb
+++ backend/test/controllers/books_controller_test.rb
@@ -1,26 +1,26 @@
 require 'test_helper'
 
-class BookControllerTest < ActionDispatch::IntegrationTest
+class BooksControllerTest < ActionDispatch::IntegrationTest
   def headers
     @headers ||= { 'Authorization': "Bearer #{fetch_token({ email: 'admin@admin.com', password: '123456' })}" }
   end
 
   # GET
   test 'deve retornar o index com sucesso' do
-    get '/api/book'
+    get '/api/books'
 
     assert_response :success
   end
 
   test 'deve retornar o index com sucesso depois de um filtro' do
-    get '/api/book?search=Autor 1'
+    get '/api/books?search=Autor 1'
 
     assert_response :success
     assert_equal 1, JSON.parse(@response.body.to_s).length
   end
 
   test 'filtrando um author que não existe, não deve retornar nada' do
-    get '/api/book?search=Autor inexistente'
+    get '/api/books?search=Autor inexistente'
 
     assert_response :success
     assert_equal 0, JSON.parse(@response.body.to_s).length
@@ -29,7 +29,7 @@ class BookControllerTest < ActionDispatch::IntegrationTest
   # POST
 
   test 'não deve ser possível criar um livro sem titulo' do
-    post '/api/book', params: {
+    post '/api/books', params: {
       author: 'The Painted Veil',
       description: 'Hello? Hello? Anybody home? Huh? Think, McFly.',
       image_url: 'https://placehold.it/100x100.png'
@@ -40,7 +40,7 @@ class BookControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'não deve ser possível criar um livro com titulo com menos de 5 caracteres' do
-    post '/api/book', params: {
+    post '/api/books', params: {
       title: 'tit',
       author: 'The Painted Veil',
       description: 'Hello? Hello? Anybody home? Huh? Think, McFly.',
@@ -52,7 +52,7 @@ class BookControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'não deve ser possível criar um livro sem autor' do
-    post '/api/book', params: {
+    post '/api/books', params: {
       title: 'titulo teste',
       description: 'Hello? Hello? Anybody home? Huh? Think, McFly.',
       image_url: 'https://placehold.it/100x100.png'
@@ -63,7 +63,7 @@ class BookControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'não deve ser possível criar um livro com nome de autor curto' do
-    post '/api/book', params: {
+    post '/api/books', params: {
       title: 'titulo teste',
       author: 'au',
       description: 'Hello? Hello? Anybody home? Huh? Think, McFly.',
@@ -75,7 +75,7 @@ class BookControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'não deve ser possível criar um livro sem imagem do livro' do
-    post '/api/book', params: {
+    post '/api/books', params: {
       title: 'titulo teste',
       author: 'autor teste',
       description: 'Hello? Hello? Anybody home? Huh? Think, McFly.'
@@ -86,7 +86,7 @@ class BookControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'deve ser possível criar um livro quando os dados estão ok' do
-    post '/api/book', params: {
+    post '/api/books', params: {
       title: 'titulo teste',
       author: 'autor teste',
       description: 'Hello? Hello? Anybody home? Huh? Think, McFly.',
@@ -100,7 +100,7 @@ class BookControllerTest < ActionDispatch::IntegrationTest
   # PUT
 
   test 'não deve ser possível atualizar um livro sem titulo' do
-    put "/api/book/#{books(:one).id}", params: {
+    put "/api/books/#{books(:one).id}", params: {
       title: '',
       author: 'autor teste',
       description: 'Hello? Hello? Anybody home? Huh? Think, McFly.',
@@ -112,7 +112,7 @@ class BookControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'não deve ser possível atualizar um livro com titulo curto' do
-    put "/api/book/#{books(:one).id}", params: {
+    put "/api/books/#{books(:one).id}", params: {
       title: 'as',
       author: 'autor teste',
       description: 'Hello? Hello? Anybody home? Huh? Think, McFly.',
@@ -124,7 +124,7 @@ class BookControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'não deve ser possível atualizar um livro sem autor' do
-    put "/api/book/#{books(:one).id}", params: {
+    put "/api/books/#{books(:one).id}", params: {
       title: 'titulo teste',
       author: '',
       description: 'Hello? Hello? Anybody home? Huh? Think, McFly.',
@@ -136,7 +136,7 @@ class BookControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'não deve ser possível atualizar um livro com autor curto' do
-    put "/api/book/#{books(:one).id}", params: {
+    put "/api/books/#{books(:one).id}", params: {
       title: 'titulo teste',
       author: 'ab',
       description: 'Hello? Hello? Anybody home? Huh? Think, McFly.',
@@ -148,7 +148,7 @@ class BookControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'não deve ser possível atualizar um livro sem imagem' do
-    put "/api/book/#{books(:one).id}", params: {
+    put "/api/books/#{books(:one).id}", params: {
       title: 'titulo teste',
       author: 'autor',
       description: 'Hello? Hello? Anybody home? Huh? Think, McFly.',
@@ -161,7 +161,7 @@ class BookControllerTest < ActionDispatch::IntegrationTest
 
   test 'deve ser possível alterar o livro se o dados estão ok' do
     id = books(:one).id
-    put "/api/book/#{id}", params: {
+    put "/api/books/#{id}", params: {
       title: 'titulo teste x',
       author: 'autor',
       description: 'Hello? Hello? Anybody home? Huh? Think, McFly.',
@@ -175,14 +175,14 @@ class BookControllerTest < ActionDispatch::IntegrationTest
 
   # DELETE
   test 'não deve ser possível deletar um registro que não existe' do
-    delete '/api/book/32145678', headers: headers
+    delete '/api/books/32145678', headers: headers
 
     assert_response :not_found
   end
 
   test 'deve ser possível deletar um livro' do
     id = books(:one).id
-    delete "/api/book/#{id}", headers: headers
+    delete "/api/books/#{id}", headers: headers
 
     assert_response :no_content
     assert_empty @response.body
